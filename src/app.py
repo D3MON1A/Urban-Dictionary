843f71fe4f8ddb723c633dbfff770bf9764cf6f8
leisyarcia <leisyarcia@gmail.com>
2020-05-05 01:13:33 +0000

    urban dictionary not completed

--- src/app.py
+++ src/app.py
@@ -10,3 +10,19 @@ API_HOST = os.getenv("API_HOST")
 API_KEY = os.getenv("API_KEY")
 
 # continue with your application here
+userInput=input("What term do you want to look for?") 
+
+import requests
+
+url = "https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=" +userInput
+
+headers = {
+    'x-rapidapi-host': API_HOST,
+    'x-rapidapi-key': API_KEY
+}
+
+response = requests.request("GET", url, headers=headers)
+response=response.json()
+#for x in response["list"]:
+
+print(response["list"][0]["definition"]) 
